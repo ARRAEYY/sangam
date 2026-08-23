c0a1f9923792f2ff030d1b9e56bff6f900c2f03e
Adil <adilreyazsk@gmail.com>
2026-08-24 01:05:47 +0530

    Update Sangam emblem to exact geometric design

--- frontend/src/components/SangamLogo.jsx
+++ frontend/src/components/SangamLogo.jsx
@@ -5,52 +5,89 @@ export function SangamEmblem({ size = 28, className = '' }) {
     <svg
       width={size}
       height={size}
-      viewBox="0 0 100 100"
+      viewBox="0 0 500 500"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={`shrink-0 ${className}`}
     >
-      {/* Central bold concentric ring */}
-      <circle cx="50" cy="50" r="28" stroke="currentColor" strokeWidth="12" fill="none" />
-      
-      {/* Outer concentric segmented arcs */}
+      {/* Central heavy ring */}
       <path
-        d="M 50 4 A 46 46 0 0 0 4 50"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M 250 72 A 178 178 0 1 0 250 428 A 178 178 0 1 0 250 72 Z M 250 142 A 108 108 0 1 1 250 358 A 108 108 0 1 1 250 142 Z"
+        fill="currentColor"
+      />
+
+      {/* Top-Right solid quadrant block */}
+      <path
+        d="M 250 0 A 250 250 0 0 1 500 250 L 452 250 A 202 202 0 0 0 250 48 Z"
+        fill="currentColor"
+      />
+
+      {/* Bottom-Left solid quadrant block */}
+      <path
+        d="M 0 250 A 250 250 0 0 0 250 500 L 250 452 A 202 202 0 0 1 48 250 Z"
+        fill="currentColor"
+      />
+
+      {/* Middle concentric thin guide arcs */}
+      <path
+        d="M 250 54 A 196 196 0 0 0 54 250"
         stroke="currentColor"
-        strokeWidth="9"
-        strokeLinecap="round"
+        strokeWidth="10"
+        strokeLinecap="square"
+        fill="none"
       />
       <path
-        d="M 4 50 A 46 46 0 0 0 68 93"
+        d="M 250 446 A 196 196 0 0 0 446 250"
         stroke="currentColor"
-        strokeWidth="9"
-        strokeLinecap="round"
+        strokeWidth="10"
+        strokeLinecap="square"
+        fill="none"
+      />
+
+      {/* Top-Left floating outer arc */}
+      <path
+        d="M 188 12 A 236 236 0 0 0 28 190"
+        stroke="currentColor"
+        strokeWidth="28"
+        strokeLinecap="square"
+        fill="none"
+      />
+
+      {/* Bottom-Right floating outer arc */}
+      <path
+        d="M 312 488 A 236 236 0 0 0 472 310"
+        stroke="currentColor"
+        strokeWidth="28"
+        strokeLinecap="square"
+        fill="none"
+      />
+
+      {/* Stepped transition notch lines */}
+      <path
+        d="M 250 48 L 250 72"
+        stroke="currentColor"
+        strokeWidth="14"
+        strokeLinecap="square"
       />
       <path
-        d="M 50 4 A 46 46 0 0 1 96 50"
+        d="M 250 428 L 250 452"
         stroke="currentColor"
-        strokeWidth="9"
-        strokeLinecap="round"
+        strokeWidth="14"
+        strokeLinecap="square"
       />
       <path
-        d="M 96 50 A 46 46 0 0 1 78 86"
+        d="M 48 250 L 72 250"
         stroke="currentColor"
-        strokeWidth="9"
-        strokeLinecap="round"
-      />
-      
-      {/* Precision concentric accent notches */}
-      <path
-        d="M 12 36 A 40 40 0 0 0 12 64"
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
+        strokeWidth="14"
+        strokeLinecap="square"
       />
       <path
-        d="M 88 36 A 40 40 0 0 1 88 64"
+        d="M 428 250 L 452 250"
         stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
+        strokeWidth="14"
+        strokeLinecap="square"
       />
     </svg>
   )
