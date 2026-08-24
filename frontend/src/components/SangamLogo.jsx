7627e4d0a469124634fdeb21c0ecfaf96c4a1fde
Adil <adilreyazsk@gmail.com>
2026-08-24 16:15:44 +0530

    style: update Sangam logo vector mark to match official brand asset

--- frontend/src/components/SangamLogo.jsx
+++ frontend/src/components/SangamLogo.jsx
@@ -5,101 +5,75 @@ export function SangamEmblem({ size = 28, className = '' }) {
     <svg
       width={size}
       height={size}
-      viewBox="0 0 500 500"
+      viewBox="0 0 200 200"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={`shrink-0 ${className}`}
     >
-      {/* Central heavy ring */}
+      {/* Central Donut Ring */}
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M 250 72 A 178 178 0 1 0 250 428 A 178 178 0 1 0 250 72 Z M 250 142 A 108 108 0 1 1 250 358 A 108 108 0 1 1 250 142 Z"
+        d="M 100 42 C 132.033 42 158 67.967 158 100 C 158 132.033 132.033 158 100 158 C 67.967 158 42 132.033 42 100 C 42 67.967 67.967 42 100 42 Z M 100 68 C 117.673 68 132 82.327 132 100 C 132 117.673 117.673 132 100 132 C 82.327 132 68 117.673 68 100 C 68 82.327 82.327 68 100 68 Z"
         fill="currentColor"
       />
 
-      {/* Top-Right solid quadrant block */}
+      {/* Middle Top-Right Solid Quadrant */}
       <path
-        d="M 250 0 A 250 250 0 0 1 500 250 L 452 250 A 202 202 0 0 0 250 48 Z"
+        d="M 100 22 A 78 78 0 0 1 178 100 L 165 100 A 65 65 0 0 0 100 35 Z"
         fill="currentColor"
       />
 
-      {/* Bottom-Left solid quadrant block */}
+      {/* Middle Bottom-Left Solid Quadrant */}
       <path
-        d="M 0 250 A 250 250 0 0 0 250 500 L 250 452 A 202 202 0 0 1 48 250 Z"
+        d="M 22 100 A 78 78 0 0 0 100 178 L 100 165 A 65 65 0 0 1 35 100 Z"
         fill="currentColor"
       />
 
-      {/* Middle concentric thin guide arcs */}
+      {/* Middle Concentric Thin Arc - Top Left */}
       <path
-        d="M 250 54 A 196 196 0 0 0 54 250"
+        d="M 100 35 A 65 65 0 0 0 35 100"
         stroke="currentColor"
-        strokeWidth="10"
-        strokeLinecap="square"
-        fill="none"
-      />
-      <path
-        d="M 250 446 A 196 196 0 0 0 446 250"
-        stroke="currentColor"
-        strokeWidth="10"
-        strokeLinecap="square"
+        strokeWidth="4"
         fill="none"
       />
 
-      {/* Top-Left floating outer arc */}
+      {/* Middle Concentric Thin Arc - Bottom Right */}
       <path
-        d="M 188 12 A 236 236 0 0 0 28 190"
+        d="M 100 165 A 65 65 0 0 0 165 100"
         stroke="currentColor"
-        strokeWidth="28"
-        strokeLinecap="square"
+        strokeWidth="4"
         fill="none"
       />
 
-      {/* Bottom-Right floating outer arc */}
+      {/* Outer Floating Arc - Top Left */}
       <path
-        d="M 312 488 A 236 236 0 0 0 472 310"
+        d="M 76 10 A 90 90 0 0 0 10 76"
         stroke="currentColor"
-        strokeWidth="28"
-        strokeLinecap="square"
+        strokeWidth="8"
+        strokeLinecap="butt"
         fill="none"
       />
 
-      {/* Stepped transition notch lines */}
+      {/* Outer Floating Arc - Bottom Right */}
       <path
-        d="M 250 48 L 250 72"
+        d="M 124 190 A 90 90 0 0 0 190 124"
         stroke="currentColor"
-        strokeWidth="14"
-        strokeLinecap="square"
-      />
-      <path
-        d="M 250 428 L 250 452"
-        stroke="currentColor"
-        strokeWidth="14"
-        strokeLinecap="square"
-      />
-      <path
-        d="M 48 250 L 72 250"
-        stroke="currentColor"
-        strokeWidth="14"
-        strokeLinecap="square"
-      />
-      <path
-        d="M 428 250 L 452 250"
-        stroke="currentColor"
-        strokeWidth="14"
-        strokeLinecap="square"
+        strokeWidth="8"
+        strokeLinecap="butt"
+        fill="none"
       />
     </svg>
   )
 }
 
-export default function SangamLogo({ emblemSize = 26, textClass = 'text-lg', className = '' }) {
+export default function SangamLogo({ emblemSize = 28, textClass = 'text-xl', className = '' }) {
   return (
     <div className={`flex items-center gap-2.5 ${className}`}>
       <span className="flex items-center justify-center text-slate-900">
         <SangamEmblem size={emblemSize} />
       </span>
-      <span className={`font-sans font-bold tracking-tight text-slate-900 ${textClass}`}>
+      <span className={`font-sans font-extrabold tracking-tight text-slate-900 ${textClass}`}>
         Sangam
       </span>
     </div>
